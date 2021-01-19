baedf041cabe819d6408b96d2ff3fdfe8df087c0
Wasim <69575827+Wasim@users.noreply.github.com>
2021-01-19 16:29:24 +0200

    better way

--- projects/number-with-comma/src/lib/number-with-comma.component.ts
+++ projects/number-with-comma/src/lib/number-with-comma.component.ts
@@ -1,12 +1,10 @@
-import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, HostBinding, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
 @Component({
   selector: 'number-input',
   template: `
-                <input [name]="name"   [readonly]="readonly" [(ngModel)]="value"
-               *ngIf="focus" (focusout)="focus=false" type="number">
-            <div *ngIf="!focus" (click)="focus=true"
-              (focusout)="focus=false">{{val ? val.toLocaleString() : 0}}</div>
+               <input [name]="name"   [readonly]="readonly" [(ngModel)]="value" (focusout)="valueChanged()" type="text">
   `,
   styles: [
   ],
@@ -18,36 +16,52 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
     }
   ]
 })
-export class NumberWithCommaComponent implements OnInit, ControlValueAccessor {
+export class NumberWithCommaComponent implements ControlValueAccessor {
   @Input() name: string;
   @Input() readonly: boolean;
 
   onChange: any = () => { }
   onTouch: any = () => { }
-  val = 0
-  focus = false;
-  constructor() { }
-
-  ngOnInit() {
+  value = "";
+  valueChanged() {  // this value is updated by programmatic changes if( val !== undefined && this.val !== val){
+    let numAfterDigit = this.value.split('.')[1];
+    this.value = this.value.split('.')[0];
+    this.value = this.value.replace(/\D/g, '');
+    this.value = this.addCommas(this.value)
+    if (numAfterDigit) {
+      numAfterDigit = numAfterDigit.replace(/\D/g, '');
+      if (numAfterDigit) {
+        this.value = this.value + '.' + numAfterDigit;
+      }
+    }
+    this.onChange(this.value)
+    this.onTouch(this.value)
   }
-
-
-  set value(val) {
-    this.val = val
-    this.onChange(val)
-    this.onTouch(val)
-  }
-
-
+  // this method sets the value programmatically
   writeValue(value: any) {
-    this.value = value
+    this.value = this.addCommas(value)
   }
-
+  // upon UI element value changes, this method gets triggered
   registerOnChange(fn: any) {
     this.onChange = fn
   }
+  // upon touching the element, this method gets triggered
   registerOnTouched(fn: any) {
     this.onTouch = fn
+  }
+
+  addCommas(number) {
+    if (isNaN(number)) {
+      return number;
+    }
+    if (number) {
+      var parts = number.toString().split(".");
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return parts.join(".");
+    }
+    else {
+      return 0;
+    }
   }
 
 }
